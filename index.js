6c8c8d812581269a3ab9e0f85c98aad2cf98c4aa
Jason Triemert <jaytriemert@gmail.com>
2023-04-27 16:26:00 -0500

    worked on shape clasess

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ inquirer.prompt([
   {
     type: "input",
     message: "Enter a color keyword (OR a hexadecimal number) for the text:",
-    name: "text-color"
+    name: "textColor"
   },
   {
     type: "list",
@@ -24,9 +24,34 @@ inquirer.prompt([
   {
     type: "input",
     message: "Enter a color keyword (OR a hexadecimal number) for the shape:",
-    name: "shape-color"
-  },
-  .then((res) => {
-    console.log(res.name, res.text-color, res.shape, res.shape-color)
-    fs.writeFileSync("logo.svg", generateLogo(res))
+    name: "shapeColor"
+  }
 ])
+  .then((res) => {
+    // console.log(res.name, res.textColor, res.shape, res.shapeColor)
+    // fs.writeFileSync("logo.svg", generateLogo(res))
+    console.log(res)
+    const logo = makeLogo(res)
+    console.log(logo)
+    // Save to file
+  })
+
+  const makeLogo = (res) => {
+    let custShape
+
+    if (data.shape === "circle") {
+      custShape = makeCircle(data.shapeColor)
+    }
+
+    return `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+
+    ${custShape}
+  
+    <text x="150" y="125" font-size="60" text-anchor="middle" fill="${data.textColor}">SVG</text>
+  
+  </svg>`
+  }
+
+const makeCircle = (color) => {
+  return `<circle cx="150" cy="100" r="80" fill="${color}" />`
+}
